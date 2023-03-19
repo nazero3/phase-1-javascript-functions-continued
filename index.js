5ef3fb0ebac4ca70bff3e0af5a09fead1210f5b8
nazero3 <nazerrabah1994@gmail.com>
2023-03-19 21:52:28 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(sth = "roller-skate") {
+    return 'This Saturday, I want to ' + sth + "!";
+}
+
+function mondayWork(sth = "go to the office") {
+    return 'This Monday, I will ' + sth + '.';
+}
+
+function wrapAdjective(flair = '*') {
+    return function (str = "special") {
+        return `You are ${flair}` + str + `${flair}!`;
+    }
+
+}
